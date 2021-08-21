5aa6e677796d4b8b18a0029c07a698ab9defa2c6
Sviatoslav Zhilichev <tom.d@newizze.com>
2021-08-21 21:42:06 +0300

    settings

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    const TYPE_ELECTRICITY = 1;
+    const TYPE_WARM = 2;
+    const TYPE_WATER = 3;
+    const TYPE_SEWERAGE = 4;
 }
